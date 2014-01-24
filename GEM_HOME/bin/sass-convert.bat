f753576932595e1e2d19d73390e64be592d1c784
Andrew Savka <a_sawka@yahoo.com>
2014-01-24 23:33:02 +0200

    Initial commit

---
+++ GEM_HOME/bin/sass-convert.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"ruby.exe" "D:/work/RAILS4~2/GEM_HOME/bin/sass-convert" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"ruby.exe" "%~dpn0" %*
---
+++ GEM_HOME/bin/tilt.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"ruby.exe" "D:/work/RAILS4~2/GEM_HOME/bin/tilt" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"ruby.exe" "%~dpn0" %*
