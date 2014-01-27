1416aa06823c741ea2193cf8ea8024d0285273e3
Andrew Savka <a_sawka@yahoo.com>
2014-01-27 22:30:22 +0200

    finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,19 +14,19 @@ describe "Static pages" do
   describe "Help page" do
     before { visit help_path }
   	it { should have_content('Help') }
-  	it { should have_title('Help') }
+  	it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }    
-  	it { should  have_content('About Us') }
-    it { should have_title('About Us') }
+  	it { should have_content('About Us') }
+    it { should have_title(full_title('About Us')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
   	it { should have_content('Contact') }
-  	it { should have_title('Contact') }
+  	it { should have_title(full_title('Contact')) }
   end  
 
 end
