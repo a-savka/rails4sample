7e0fb2f462f816d5a8dcf34aac59bee38b90cae1
Andrew Savka <a_sawka@yahoo.com>
2014-02-03 22:45:43 +0200

    Added authorization

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -26,4 +26,17 @@ module SessionsHelper
 		!current_user.nil?
 	end
 
+	def correct_user?(user)
+		user == current_user
+	end
+
+	def redirect_back_or(default)
+		redirect_to session[:return_to] || default
+		session.delete(:return_to)
+	end
+
+	def store_location
+		session[:return_to] = request.url if request.get?
+	end
+
 end
