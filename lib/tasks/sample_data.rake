c28e4d1dcf7406c22213d4ec0567fc43f1a656b2
Andrew Savka <a_sawka@yahoo.com>
2014-02-15 08:44:19 +0200

    User following

--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -1,30 +1,48 @@
 namespace :db do	
 	desc "Fill database with sample data"
 	task populate: :environment do
-		User.create!(
-			name: "Example User",
-			email: "example@railstutorial.org",
-			password: "foobar",
-			password_confirmation: "foobar",
-			admin: true)
-		99.times do |n|
-			name = Faker::Name.name
-			email = "example-#{n+1}@railstutorial.org"
-			password = "password"
-			User.create!(
-				name: name,
-				email: email,
-				password: password,
-				password_confirmation: password)
-		end
-
-		users = User.all(limit: 6)
-		50.times do
-			content = Faker::Lorem.sentence(5)
-			users.each do |user|
-				user.microposts.create!(content: content)
-			end
-		end
-		
+		make_users
+		make_microposts
+		make_relationships
 	end
+end
+
+def make_users
+
+	User.create!(
+		name: "Example User",
+		email: "example@railstutorial.org",
+		password: "foobar",
+		password_confirmation: "foobar",
+		admin: true)
+
+	99.times do |n|
+		name = Faker::Name.name
+		email = "example-#{n+1}@railstutorial.org"
+		password = "password"
+		User.create!(
+			name: name,
+			email: email,
+			password: password,
+			password_confirmation: password)
+	end	
+
+end
+
+def make_microposts
+	users = User.all(limit: 6)
+	50.times do
+		content = Faker::Lorem.sentence(5)
+		users.each do |user|
+			user.microposts.create!(content: content)
+		end
+	end	
+end
+
+def make_relationships 
+	user = User.first
+	followed_users = User.all[2..50]
+	followers = User.all[3..40]
+	followed_users.each { |followed_user| user.follow!(followed_user) }
+	followers.each { |follower| follower.follow!(user) }
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,10 +5,48 @@ describe "Static pages" do
   subject { page }
 
   describe "Home page" do
+
     before { visit root_path }
     it { should have_content('Sample App') }
     it { should have_title(full_title('')) }
     it { should_not have_title('Home') }
+
+
+    describe "for signed in users" do
+
+      let(:user) { FactoryGirl.create(:user) }
+
+      before do
+        31.times do |i|
+          FactoryGirl.create(:micropost, user: user, content: "Lorem #{i}")
+        end
+        sign_in user
+        visit root_path
+      end
+
+      it { should have_content("micropost".pluralize(user.feed.count)) }
+      it { should have_selector("div.pagination") }
+
+      it "should render the user's feed" do
+        user.feed.paginate(page: 1).each do |item|
+          expect(page).to have_selector("li##{item.id}", text: item.content)
+        end
+      end
+
+      describe "followers/following counts" do
+
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+
+      end
+
+    end    
   end
 
   describe "Help page" do
@@ -29,27 +67,5 @@ describe "Static pages" do
   	it { should have_title(full_title('Contact')) }
   end  
 
-  describe "for signed in users" do
 
-    let(:user) { FactoryGirl.create(:user) }
-
-    before do
-      31.times do |i|
-        FactoryGirl.create(:micropost, user: user, content: "Lorem #{i}")
-      end
-      sign_in user
-      visit root_path
-    end
-
-    it { should have_content("micropost".pluralize(user.feed.count)) }
-    it { should have_selector("div.pagination") }
-
-    it "should render the user's feed" do
-      user.feed.paginate(page: 1).each do |item|
-        expect(page).to have_selector("li##{item.id}", text: item.content)
-      end
-    end
-
-  end
-  
 end
